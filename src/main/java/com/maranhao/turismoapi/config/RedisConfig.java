87e5b93106788f971974fe265f8d26bb814118cb
Aécio <aetius.studio@gmail.com>
2026-07-04 00:13:55 -0300

    "feat: implementa cache com Redis"

--- src/main/java/com/maranhao/turismoapi/config/RedisConfig.java
+++ src/main/java/com/maranhao/turismoapi/config/RedisConfig.java
@@ -5,8 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import tools.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
 
@@ -15,12 +16,17 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        GenericJacksonJsonRedisSerializer serializer =
+                new GenericJacksonJsonRedisSerializer(new ObjectMapper());
+
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair
-                                .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                                .fromSerializer(serializer)
                 );
+
+
         return  RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
                 .build();
